e7899963cc00e1b3575e702ad231a7b21662a9fe
Jean Lourenço <jeancarlos.bnu@gmail.com>
2022-07-10 14:29:00 -0300

    Add algo based on tile possibilities

--- Program.cs
+++ Program.cs
@@ -2,32 +2,44 @@
 var board = new Tile[20, 80];
 var allTilePossibilites = Enum.GetValues<TileType>().ToArray();
 
-var initialY = board.GetLength(0)/2;
-var initialX = board.GetLength(1)/2;
-ref var initial = ref board[initialY, initialX];
-initial.SetTile(GetRandomTileType(random));
-
 for (var y = 0; y < board.GetLength(0); y++)
 {
     for (var x = 0; x < board.GetLength(1); x++)
     {
-        if (initialY == 0 && initialX == 0) continue;
-
-        var possibleTiles = GetTileTypePossibilities(board, y, x);
-        var newTile = CreateRandomTile(random, possibleTiles);
-
-        if (newTile == null)
-        {
-            System.Console.WriteLine($"Sem combinações possívels y={y} x={x}");
-            DrawBoard(board);
-            return;
-        }
-
-        board[y, x] = newTile;
+        board[y, x] = new Tile(y, x);
     }
 }
 
+var initialY = board.GetLength(0)/2;
+var initialX = board.GetLength(1)/2;
+ref var initial = ref board[initialY, initialX];
+initial.SetTile(GetRandomTileType(random));
+ReducePossibleNeighboursTiles(board, initialY, initialX);
+
+while (Flatten(board).Any(x => x.TileType == TileType.None))
+{
+    var min = Flatten(board)
+        .Where(x => x.TileType == TileType.None)
+        .GroupBy(x => x.NumberOfPossibleTiles)
+        .MinBy(x => x.Key)!
+        .ToArray();
+
+    var n = min[(new Random()).Next(0, min.Length)];
+    n.SetTile(GetRandomTileType(random, n.PossibleTiles));
+    ReducePossibleNeighboursTiles(board, n.Y, n.X);
+}
+
+Console.Clear();
 DrawBoard(board);
+
+
+IEnumerable<T> Flatten<T>(T[,] map) {
+  for (int row = 0; row < map.GetLength(0); row++) {
+    for (int col = 0; col < map.GetLength(1); col++) {
+      yield return map[row,col];
+    }
+  }
+}
 
 void DrawBoard(Tile[,] tiles)
 {
@@ -36,7 +48,7 @@ void DrawBoard(Tile[,] tiles)
     {
         for (var x = 0; x < tiles.GetLength(1); x++)
         {
-            Console.Write(tiles[y, x]?.ToString() ?? ".");
+            Console.Write(tiles[y, x].ToString() ?? ".");
         }
         Console.ResetColor();
         Console.WriteLine();
@@ -58,41 +70,46 @@ TileType GetRandomTileType(Random r, TileType[]? bounds = null)
     return values[r.Next(values.Length)];
 }
 
-TileType[] GetTileTypePossibilities(Tile[,] tiles, int y, int x)
+void ReducePossibleNeighboursTiles(Tile[,] b, int y, int x)
 {
-    return
-        GetTileNeighbours(y - 1, x - 1)
-        .Intersect(GetTileNeighbours(y - 1, x))
-        .Intersect(GetTileNeighbours(y - 1, x + 1))
-        .Intersect(GetTileNeighbours(y, x - 1))
-        .Intersect(GetTileNeighbours(y, x))
-        .Intersect(GetTileNeighbours(y, x + 1))
-        .Intersect(GetTileNeighbours(y + 1, x - 1))
-        .Intersect(GetTileNeighbours(y + 1, x))
-        .Intersect(GetTileNeighbours(y + 1, x + 1))
-        .ToArray();
+    var possibilies = b[y, x].GetPossibleNeighbours();
 
-    TileType[] GetTileNeighbours(int cy, int cx)
+    GetTile(y - 1, x - 1)?.ReducePossibleTiles(possibilies);
+    GetTile(y - 1, x)?.ReducePossibleTiles(possibilies);
+    GetTile(y - 1, x + 1)?.ReducePossibleTiles(possibilies);
+
+    GetTile(y, x - 1)?.ReducePossibleTiles(possibilies);
+    GetTile(y, x + 1)?.ReducePossibleTiles(possibilies);
+
+    GetTile(y + 1, x - 1)?.ReducePossibleTiles(possibilies);
+    GetTile(y + 1, x)?.ReducePossibleTiles(possibilies);
+    GetTile(y + 1, x + 1)?.ReducePossibleTiles(possibilies);
+
+    Tile? GetTile(int cy, int cx)
     {
-        if (cy < 0 || cy >= tiles.GetLength(0) || cx < 0 || cx >= tiles.GetLength(1))
-            return allTilePossibilites;
+        if (cy < 0 || cy >= b.GetLength(0) || cx < 0 || cx >= b.GetLength(1))
+            return null;
 
-        return tiles[cy, cx]?.GetPossibleNeighbours() ?? allTilePossibilites;
+        return b[cy, cx];
     }
 }
 
 public enum TileType { None, Mountain, Grass, Sand, Water, }
 
-public struct Tile
+public class Tile
 {
     public TileType TileType { get; private set; }
     public TileType[] PossibleTiles { get; private set; }
+    public int Y { get; }
+    public int X { get; }
     public int NumberOfPossibleTiles => PossibleTiles.Length;
 
-    public Tile()
+    public Tile(int y, int x)
     {
         TileType = TileType.None;
         PossibleTiles = Enum.GetValues<TileType>().Except(new[] {TileType.None}).ToArray();
+        Y = y;
+        X = x;
     }
 
     public TileType[] GetPossibleNeighbours()
@@ -110,7 +127,16 @@ public struct Tile
 
     public void ReducePossibleTiles(TileType[] types)
     {
-        PossibleTiles = PossibleTiles.Intersect(types).ToArray();
+        try
+        {
+            if (types != null && types.Length > 0)
+                PossibleTiles = PossibleTiles.Intersect(types).ToArray();
+
+        }
+        catch (Exception)
+        {
+            Console.WriteLine($"PossibleTiles {PossibleTiles?.Length ?? -1}, Types: {types?.Length ?? -1}");
+        }
     }
 
     public void SetTile(TileType type)
@@ -145,6 +171,9 @@ public struct Tile
                 Console.BackgroundColor = ConsoleColor.Blue;
                 Console.ForegroundColor = ConsoleColor.Blue;
                 return "W";
+            case TileType.None:
+                Console.ResetColor();
+                return " ";
             default:
                 Console.BackgroundColor = ConsoleColor.Red;
                 Console.ForegroundColor = ConsoleColor.Red;
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ### v0.1, usando scan linear pela matriz
 ![img](imgs/v0.1.png)
+
+### v0.2, usando prioridade por possibilidades de campos
+![img](imgs/v0.2.png)
